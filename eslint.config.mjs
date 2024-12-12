17a478b33415d931dbfee060a52ebe82e325678e
matheusscode <matheussdev3@gmail.com>
2024-12-11 21:34:16 -0400

    chore: add new extension in eslint.config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.{jss,cjs,ts}"] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
